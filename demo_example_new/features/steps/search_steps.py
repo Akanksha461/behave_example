de10bf0e0735497651bf571ef2bc95ba1a47c2f8
Akanksha verma <ruhi461@gmail.com>
2017-07-05 15:46:11 +0530

    Update search_steps.py

--- demo_example_new/features/steps/search_steps.py
+++ demo_example_new/features/steps/search_steps.py
@@ -2,22 +2,22 @@ from nose.tools import assert_equal, assert_true,assert_in
 from selenium.webdriver.common.by import By
 import time
 
-@step('I navigate to the PyPi Home page')
-def step_impl(context):
-    context.home_page.navigate("https://pypi.python.org/pypi")
-    assert_equal(context.home_page.get_page_title(), "PyPI - the Python Package Index : Python Package Index")
+# @step('I navigate to the PyPi Home page')
+# def step_impl(context):
+#     context.home_page.navigate("https://pypi.python.org/pypi")
+#     assert_equal(context.home_page.get_page_title(), "PyPI - the Python Package Index : Python Package Index")
 
-@step('I search for "{search_term}"')
-def step_impl(context, search_term):
-    context.home_page.search(search_term)
+# @step('I search for "{search_term}"')
+# def step_impl(context, search_term):
+#     context.home_page.search(search_term)
 
-@step('I am taken to the PyPi Search Results page')
-def step_impl(context):
-    assert_equal(context.search_results_page.get_page_title(), "Index of Packages Matching 'behave' : Python Package Index")
+# @step('I am taken to the PyPi Search Results page')
+# def step_impl(context):
+#     assert_equal(context.search_results_page.get_page_title(), "Index of Packages Matching 'behave' : Python Package Index")
 
-@step('I see a search result "{search_result}"')
-def step_impl(context, search_result):
-    assert_true(context.search_results_page.find_search_result(search_result))
+# @step('I see a search result "{search_result}"')
+# def step_impl(context, search_result):
+#     assert_true(context.search_results_page.find_search_result(search_result))
 
 @step('I navigate to StackOverflow homepage')
 def step_impl(context):
